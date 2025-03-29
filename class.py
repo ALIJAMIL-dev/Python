116137fc84cb5ca6297534beea19cf2d794bac2f
ALI JAMIL <alijamil@outlook.com.tr>
2025-03-29 22:42:20 +0300

    Class

---
+++ class.py
@@ -0,0 +1,13 @@
+# Class
+class Product:
+    pass
+
+# Instance, Object
+
+product1 = Product()
+product2 = Product()
+
+print(type(product1))  # <class '__main__.Product'>
+print(type(product2))  # <class '__main__.Product'>
+
+print(product1)
