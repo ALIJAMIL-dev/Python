8925daeb5ab6f84084a33fa74839e98a391ba901
ALI JAMIL <alijamil@outlook.com.tr>
2025-03-31 13:24:20 +0300

    added instance methods

--- class.py
+++ class.py
@@ -1,13 +1,31 @@
-# Class
 class Product:
-    pass
+    # Attributes & Properties
+    def __init__(self, name, price, isActive=True):
+        self.name = name
+        self.price = price
+        self.isActive = isActive
+    
+    # Instance Method
+    def get_product(self):
+        return f"Product: {self.name}, Price: {self.kdv_price}, Active: {self.isActive}"
+    
+    def kdv_price(self):
+        return self.price * 1.18
 
-# Instance, Object
+# Instance & Object
 
-product1 = Product()
-product2 = Product()
+product1 = Product("Lenovo V15 G4 IRU", 360, True)
+product2 = Product("Dell Inspiron 3515", 450, True)
+product3 = Product("Acer Aspire 5", 500, True)
+product4 = Product("Asus Vivobook 15", 550, True)
+product5 = Product("Samsung S25 Ultra+", 700, True)
 
-print(type(product1))  # <class '__main__.Product'>
-print(type(product2))  # <class '__main__.Product'>
+products = [product1, product2, product3, product4, product5]
 
-print(product1)
+#for i in products:
+#    if i.isActive:
+#        print("Product: {self.name}, Price: {self.price}, Active: {self.isActive}")
+
+for i in products:
+    if i.isActive:
+        print(i.get_product())
