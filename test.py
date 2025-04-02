055aed61a705db7cc60ebc170f62f6095f9be4c4
ALI JAMIL <alijamil@outlook.com.tr>
2025-04-02 11:28:09 +0300

    Fucking Appppppppppppps

---
+++ test.py
@@ -0,0 +1,20 @@
+class App:
+    class Calculator:
+        def main():
+            pass
+        def plus():
+            pass
+        def manus():
+            pass
+        def multiply():
+            pass
+        def division():
+            pass
+    class School:
+        pass
+    class FuCKYOU:
+        def __init__(self):
+            n = 0
+            while n <= 1:
+                print("Fuck You Everyone") 
+        
