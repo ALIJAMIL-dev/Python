953f67a141fbbc0f697ece309206bcf154bd7e2a
ALI JAMIL <alijamil@outlook.com.tr>
2025-04-21 12:54:28 +0300

    Mided python sqlllllllll

---
+++ Databasing/sql_2.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+connection = sqlite3.connect('D:/Python/Databasing/chinook.db')
+cursor = connection.cursor()
+
+sql = "INSERT INTO genres (Name) VALUES ('Fuck You')"
+cursor.execute(sql)
+connection.commit()
+# cursor.execute('SELECT * FROM customers where city="Oslo"')  # Corrected table name
+# result = cursor.fetchall()
+
+# for customer in result:
+#     print(customer[1])
+
+
+connection.close()
+print("Database connection established successfully.")
