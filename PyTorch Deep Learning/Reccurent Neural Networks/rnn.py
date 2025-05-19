bd43fc23bea62f41b800ffbfbd2893d555d85469
ALI JAMIL <alijamil@outlook.com.tr>
2025-05-19 14:14:40 +0300

    Adding Data models and datasets to .gitignore + PyTorch Recurrent Neural Networks RNN Create Data and Visualize

---
+++ PyTorch Deep Learning/Reccurent Neural Networks/rnn.py
@@ -0,0 +1,40 @@
+# %% Create the Data and Visualize
+import torch
+import torchvision
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+
+def gen_data(seq_length = 50, num_samples = 1000):
+    x = np.linspace(0, 100, num_samples)
+    y = np.sin(x)
+    sequence = []
+    targets = []
+
+    for i in range(len(x)-seq_length):
+       sequence.append(y[i:i+seq_length])
+       targets.append(y[i+seq_length])
+
+    plt.figure(figsize=(8, 4))
+    plt.plot(x, y, label="sin(t)", color="b", linewidth = 2)
+    plt.title("Sinus Dalga Graphic")
+    plt.xlabel("Time(Radition)")
+    plt.ylabel("Genlik")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    return np.array(sequence), np.array(targets)
+
+sequence, targets = gen_data() 
+
+# %% Create RNN Model
+
+
+
+# %% Training
+
+
+# %% RNN Test and Evaluation
+
+
