cfbfc3ce66ef0d9b09d227bb1732decafdf88306
ALI JAMIL <alijamil@outlook.com.tr>
2026-01-24 17:47:03 +0300

    PyTorch - LSTM - Hyperparameter Tuning

--- AI & ML/PyTorch Deep Learning/Long Short Term Memory/lstm.py
+++ AI & ML/PyTorch Deep Learning/Long Short Term Memory/lstm.py
@@ -53,8 +53,59 @@ class LSTM(nn.Module):
         output = self.fc(lstm_out.view(1,-1))
         return output
 
+model = LSTM(len(vocab), embedding_dim=8, hidden_dim=32)
+
 # %% Hyperparameter Tuning
 
+# Word list -> Tensor
+def prepare_sequence(seq, to_ix):
+    return torch.tensor([to_ix[w] for w in seq], dtype= torch.long)
+
+# Hyperparameter Tuning Combinations Determination
+embedding_sizes = [8, 16] # Embedding sizes to try
+hidden_sizes = [32, 64] # Hidden layer sizes to try
+learning_rates = [0.01, 0.005] # Learning Rate
+
+best_loss = float("inf") # Variable to store the lowest loss value
+best_params = {} # Empty dictionary to store best parameters
+
+print("Hyperparameter tuning starting...")
+
+# Grid Search
+for emb_size, hidden_size, lr in product(embedding_sizes, hidden_sizes, learning_rates):
+    print(f"Test: Embedding: {emb_size}, Hidden: {hidden_size}, Learning Rate: {lr}")
+
+    # Define Model
+    model = LSTM(len(vocab), emb_size, hidden_size) # Create model with selected Parameters
+    loss_function = nn.CrossEntropyLoss() # Entropy loss function
+    optimizer = optim.Adam(model.parameters(), lr = lr) # Adam optimizer with selected lr
+
+    epochs = 50
+    total_loss = 0
+    for epoch in range(epochs):
+        epoch_loss = 0.0 # Resets Epoch Loss in the beginning
+        for word, next_word in data:
+            model.zero_grad() # Resets Grads
+            input_tensor = prepare_sequence([word], w2i) # Transform Input to Tensor
+            target_tensor = prepare_sequence([next_word], w2i) # Transform Target Word to Tensor
+            output = model(input_tensor) # Prediction
+            loss = loss_function(output, target_tensor) 
+            loss.backward() # Apply Backpropagation
+            optimizer.step() # Update Parameters
+            epoch_loss += loss.item()
+
+        if epoch % 10 == 0:
+            print(f"Epoch: {epoch}, Loss: {epoch_loss:.5f}")  
+        total_loss = epoch_loss
+        # Save Best Model
+    if total_loss < best_loss:
+        best_loss = total_loss
+        best_params = {"embedding_dim": emb_size, "hidden_dim": hidden_size, "learning_rate": lr}
+
+    print()
+
+
+print(f"Best params: {best_params}")
 
 # %% LSTM Training
 
@@ -62,5 +113,5 @@ class LSTM(nn.Module):
 
 # %% Test and Evaluation
 
-model = LSTM(len(vocab), embedding_dim=8, hidden_dim=32)
+
 
